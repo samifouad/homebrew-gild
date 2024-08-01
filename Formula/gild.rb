88ee1dad8da80a6611f47b359c9d5fd73473b525
Sami Fouad <6378290+samifouad@users.noreply.github.com>
2024-07-31 23:39:22 -0700

    Create gild.rb

---
+++ Formula/gild.rb
@@ -0,0 +1,16 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+class Gild < Formula
+  desc "infrastructure as code"
+  homepage "https://gild.gg"
+  url "https://github.com/samifouad/gild-cli/releases/download/v0.2.0/gild.tar.gz"
+  sha256 "52ae8f572bf5b3aeb608a7e1b9f0da1c1c8e921e8caca7a6123b7b0e0940c3ff"
+  license "Apache2.0"
+  version "0.2.0"
+
+  # depends_on "cmake" => :build
+
+  def install
+    bin.install "gild"
+  end
+end
