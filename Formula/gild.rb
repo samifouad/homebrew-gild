1eba525a7cc2938c51880452d0b2512691ad4ceb
Sami Fouad <6378290+samifouad@users.noreply.github.com>
2024-07-31 23:52:20 -0700

    Update gild.rb

--- Formula/gild.rb
+++ Formula/gild.rb
@@ -3,10 +3,10 @@
 class Gild < Formula
   desc "infrastructure as code"
   homepage "https://gild.gg"
-  url "https://github.com/samifouad/gild-cli/releases/download/v0.2.0/gild.tar.gz"
+  url "https://github.com/samifouad/gild-cli/releases/download/v0.2.1/gild.tar.gz"
   sha256 "52ae8f572bf5b3aeb608a7e1b9f0da1c1c8e921e8caca7a6123b7b0e0940c3ff"
   license "Apache2.0"
-  version "0.2.0"
+  version "0.2.1"
 
   # depends_on "cmake" => :build
 
