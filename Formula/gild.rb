e609ecc36a183aa75a957c9693c590b5b8b03616
Sami Fouad <sfouad@gmail.com>
2024-09-17 05:44:40 +0000

    automated version bump to v0.8.3

--- Formula/gild.rb
+++ Formula/gild.rb
@@ -1,20 +1,20 @@
 class Gild < Formula
   desc "Infrastructure as code"
   homepage "https://gild.gg"
-  version "v0.8.1"
+  version "v0.8.3"
   license "Apache-2.0"
 
   if OS.mac?
     if Hardware::CPU.intel?
-      url "https://github.com/samifouad/gild-cli/releases/download/v0.8.1/gild_cli_v0.8.1_darwin_x64.tar.gz"
-      sha256 "db6808dd6a798002a93758ea0e11321695e99c47f4487d1eb0a37b964012d7f9"
+      url "https://github.com/samifouad/gild-cli/releases/download/v0.8.3/gild-cli_v0.8.3_darwin_x64.tar.gz"
+      sha256 "c03eb661bfb3372ce89090b8174558df5acc8acaf7549e35304b1a33f376f589"
     elsif Hardware::CPU.arm?
-      url "https://github.com/samifouad/gild-cli/releases/download/v0.8.1/gild_cli_v0.8.1_darwin_arm64.tar.gz"
-      sha256 "9ec7ce0d8fa149b268d61946a1a7d949b7eb99a0e5c54dfb8e286d39ed5639f7"
+      url "https://github.com/samifouad/gild-cli/releases/download/v0.8.3/gild-cli_v0.8.3_darwin_arm64.tar.gz"
+      sha256 "398b536580e5aacdf158997f869407e3aa5e4477317e899d3683263369845461"
     end
   elsif OS.linux?
-    url "https://github.com/samifouad/gild-cli/releases/download/v0.8.1/gild_cli_v0.8.1_linux_x64.tar.gz"
-    sha256 "5be59dc72aa8ae0bf68bafea233a56fa561195e3903046fc6d33aa8d53f8851e"
+    url "https://github.com/samifouad/gild-cli/releases/download/v0.8.3/gild-cli_v0.8.3_linux_x64.tar.gz"
+    sha256 "774ff8ceb389f901b0a733453e8d37d976f495cfa528659c42aa9ce36f9d3f62"
   end
 
   def install
